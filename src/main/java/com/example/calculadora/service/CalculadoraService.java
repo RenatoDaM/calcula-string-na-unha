90327e63a5a7d99b63efaa93f2b22c2ea1d6bfab
Renato <renaato.d.m@gmail.com>
2023-04-21 03:23:58 -0300

    Atualizei para um código mais modularizado, facilitando expansões futuras. Separei em diferentes funções, extrair operadores, extrair numeros, tratar string, calcular entre as duas listas que por sua vez chama o calculo em si que está em EnumOperacao

--- src/main/java/com/example/calculadora/service/CalculadoraService.java
+++ src/main/java/com/example/calculadora/service/CalculadoraService.java
@@ -1,10 +1,7 @@
 package com.example.calculadora.service;
 
-import com.example.calculadora.exception.OperadorInvalidoException;
 import com.example.calculadora.util.calculadora.string.Calculadora;
 import org.springframework.stereotype.Service;
-import java.util.*;
-import java.util.regex.Pattern;
 
 @Service
 public class CalculadoraService {
@@ -12,57 +9,8 @@ public class CalculadoraService {
     %2B = +
     */
     public Double calcularEntreDoisNumeros(String equacao) throws Exception {
-        var resultadoEmString = equacao.replace("%2B", "+");
-        resultadoEmString = resultadoEmString.trim();
-        System.out.println(resultadoEmString);
-
-
-        var verificarSinalDoPrimeiro = Pattern.compile("^[0-9]");
-        var matcherVerificarSinal = verificarSinalDoPrimeiro.matcher(resultadoEmString);
-        if (matcherVerificarSinal.find()) {
-            StringBuilder builder = new StringBuilder(resultadoEmString);
-            builder.insert(0, "+");
-            resultadoEmString = builder.toString();
-        }
-
-        var patternOperador = Pattern.compile("([-+*/])");
-        var patternNumero = Pattern.compile("(\\d+)");
-        var matcherOperador = patternOperador.matcher(resultadoEmString);
-        var matcherNumero = patternNumero.matcher(resultadoEmString);
-        List<Double> numberList = new ArrayList<>();
-        List<Optional<String>> operatorsList = new ArrayList<>();
-
-
-        // O programa ainda não respeita a ordem das operações, ele é funcional apenas para realizar
-        // uma operação básica entre dois números.
-        while (matcherNumero.find()) {
-            numberList.add(Double.parseDouble(Optional.ofNullable(matcherNumero.group()).get()));
-        }
-
-        boolean isFirstIteration = true;
-        while (matcherOperador.find()) {
-
-            if (matcherOperador.group().equals("-") && isFirstIteration) {
-                System.out.println("trueeee");
-                numberList.set(0, numberList.get(0) - numberList.get(0)*2);
-            }
-            isFirstIteration = false;
-            operatorsList.add(Optional.ofNullable(matcherOperador.group()));
-
-        }
-
-        // É possível que a expansão desse aplicativo seja ainda usando o Calculadora.calcular, quebrando uma
-        // equação em várias operações menores, após a quebra e ordenação irá chamar Calculadora.calcular.
-
-        return aplicarOperacaoEntreDoisNumeros(operatorsList, numberList);
-
+        Calculadora calculadora = new Calculadora();
+        return calculadora.calcular(equacao);
     }
 
-    private Double aplicarOperacaoEntreDoisNumeros(List<Optional<String>> operador, List<Double> listaNumero) throws OperadorInvalidoException {
-        if (operador.isEmpty()) throw new OperadorInvalidoException("Operador não reconhecido");
-        if (listaNumero.size() <= 1 || listaNumero.size() > 2) throw new OperadorInvalidoException("Não foi possível encontrar 2 números válidos apenas");
-        if (operador.size() > 2) throw new OperadorInvalidoException("Foi encontrada uma operação inválida");
-        var calculadora = new Calculadora();
-        return calculadora.calcular(listaNumero.get(0), operador.get(1).get(), listaNumero.get(1));
-    }
 }
--- src/main/java/com/example/calculadora/util/calculadora/string/Calculadora.java
+++ src/main/java/com/example/calculadora/util/calculadora/string/Calculadora.java
@@ -1,8 +1,10 @@
 package com.example.calculadora.util.calculadora.string;
 
+import com.example.calculadora.exception.OperadorInvalidoException;
 import com.example.calculadora.util.calculadora.string.operacoes.*;
-import java.util.HashMap;
-import java.util.Map;
+
+import java.util.*;
+import java.util.regex.Pattern;
 
 public class Calculadora {
     Map<String, OperacaoStrategy> relacaoOperadorOperacao = new HashMap<>();
@@ -15,10 +17,65 @@ public class Calculadora {
         relacaoOperadorOperacao.put(novoOperador, novaOperacao);
     }
 
-    public Double calcular(Double valor1, String operador2, Double valor2) {
-        OperacaoEnum calcular = OperacaoEnum.informarOperadorParaCalcular(operador2);
+    // O programa ainda não respeita a ordem das operações, ele é funcional apenas para realizar
+    // uma operação básica entre dois números.
+    public Double calcular(String equacao) throws OperadorInvalidoException {
+        var expressao = equacao.replace("%2B", "+");
+        expressao = expressao.trim();
 
+        // Insere um sinal positivo no primeiro número caso ele seja positivo e já não possua um sinal de +
+        if (Character.isDigit(expressao.charAt(0))) {
+            expressao = "+" + expressao;
+        }
+
+        List<Double> numberList = extrairNumeros(expressao);
+        List<Optional<String>> operatorsList = extrairOperadores(expressao);
+
+        // Verifica se o primeiro número da string é negativo
+        if (operatorsList.get(0).get().equals("-")) {
+            numberList.set(0, numberList.get(0) * -1);
+        }
+
+        // É possível que a expansão desse aplicativo seja quebrando uma
+        // equação em várias operações menores, possivelmente ordenando em uma lista.
+        return aplicarOperacoesEntreListas(operatorsList, numberList);
+    }
+
+    private Double aplicarOperacoesEntreListas(List<Optional<String>> operador, List<Double> listaNumero) throws OperadorInvalidoException {
+        if (operador.isEmpty()) throw new OperadorInvalidoException("Operador não reconhecido");
+        if (listaNumero.size() <= 1 || listaNumero.size() > 2) throw new OperadorInvalidoException("Não foi possível encontrar 2 números válidos apenas");
+        if (operador.size() > 2) throw new OperadorInvalidoException("Foi encontrada uma operação inválida");
+        return aplicarOperacao(listaNumero.get(0), operador.get(1).get(), listaNumero.get(1));
+    }
+
+
+    public Double aplicarOperacao(Double valor1, String operador2, Double valor2) {
+        OperacaoEnum calcular = OperacaoEnum.informarOperadorParaCalcular(operador2);
         if (calcular == null) throw new IllegalArgumentException("Operador inválido.");
         return calcular.calcular(valor1, valor2);
+    }
+
+    private List<Double> extrairNumeros(String equacao) {
+        var pattern = Pattern.compile("(\\d+)");
+        var matcher = pattern.matcher(equacao);
+        List<Double> numeros = new ArrayList<>();
+
+        while (matcher.find()) {
+            numeros.add(Double.parseDouble(Optional.ofNullable(matcher.group()).get()));
+        }
+
+        return numeros;
+    }
+
+    private List<Optional<String>> extrairOperadores(String equacao) {
+        var pattern = Pattern.compile("([-+*/])");
+        var matcher = pattern.matcher(equacao);
+        List<Optional<String>> operadores = new ArrayList<>();
+
+        while (matcher.find()) {
+            operadores.add(Optional.ofNullable(matcher.group()));
+        }
+
+        return operadores;
     }
 }
